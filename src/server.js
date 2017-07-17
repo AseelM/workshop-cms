1126297bae8a215aebb10bf34d46b1f41627a625
AseelM <aseelmustafa@outlook.com>
2017-07-17 15:34:56 +0100

    respond with index.html file

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const fs = require('fs');
+
 const server = http.createServer(handler);
 var message = "yaaay working with Bart for the 1st time! yiiippppy"
 
@@ -7,6 +9,35 @@ server.listen(3000, function(){
 });
 function handler(request, response) {
 response.writeHead(200, {'Content-Type': 'text/html'} );
-response.write(message);
-response.end();
+// response.write(message);
+// response.end();
+var endpoint = request.url;
+if (endpoint === "/"){
+  response.writeHead(200, {'Content-Type': 'text/html'} );
+  fs.readFile(__dirname + "/../public/index.html", function(error, file) {
+    if (error){
+      console.log(error);
+      return;
+    }
+    response.end(file);
+
+  })
 }
+}
+
+// }
+// console.log(endpoint);
+// var method = request.method;
+// console.log(method);
+// if (endpoint === "/node"){
+//   response.write("node");
+//   // response.end();
+// }else if (endpoint === "/girls"){
+//   response.write("girls");
+//   // response.end();
+// }else {
+//   response.write("not an endpoint");
+//   // response.end();
+// }
+// response.end();
+// }
