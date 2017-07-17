029d8b17ec68a53d6605da5a283a8a44b65c58d6
AseelM <aseelmustafa@outlook.com>
2017-07-17 15:07:55 +0100

    added a handler function

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const http = require('http');
-const server = http.createServer();
+const server = http.createServer(handler);
+var message = "yaaay working with Bart for the 1st time! yiiippppy"
 
 server.listen(3000, function(){
   console.log('server is listening on port 3000, and ready to receive requests!');
 });
+function handler(request, response) {
+response.writeHead(200, {'Content-Type': 'text/html'} );
+response.write(message);
+response.end();
+}
