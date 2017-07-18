73d34af0753e51b209892d0fba17829bf301dc72
Bart Bucknill <bartbucknill@hotmail.com>
2017-07-18 12:30:08 +0100

    accept form submission and redirect to home

--- src/server.js
+++ src/server.js
@@ -1,49 +1,62 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
+const qs = require("querystring");
 
 const server = http.createServer(handler);
-var message = "yaaay working with Bart for the 1st time! yiiippppy"
+var message = "yaaay working with Bart for the 1st time! yiiippppy";
 
-server.listen(3000, function(){
-  console.log('server is listening on port 3000, and ready to receive requests!');
+server.listen(3000, function() {
+  console.log(
+    "server is listening on port 3000, and ready to receive requests!"
+  );
 });
 function handler(request, response) {
-response.writeHead(200, {'Content-Type': 'text/html'} );
-// response.write(message);
-// response.end();
-var endpoint = request.url;
-if (endpoint === "/"){
-  response.writeHead(200, {'Content-Type': 'text/html'} );
-  fs.readFile(__dirname + "/../public/index.html", function(error, file) {
-    if (error){
-      console.log(error);
-      return;
-    }
-    response.end(file);
-
-  })
-}else if (endpoint === "/main.css"){
-  response.writeHead(200, {'Content-Type': 'text/css'} );
-  fs.readFile(__dirname + "/../public/main.css", function(error, file) {
-    if (error){
-      console.log(error);
-      return;
-    }
-    response.end(file);
-
-  })}
-else if (endpoint === "/img/image.jpg"){
-  response.writeHead(200, {'Content-Type': 'image/jpg'} );
-  fs.readFile(__dirname + "/../public/img/image.jpg", function(error, file) {
-    if (error){
-      console.log(error);
-      return;
-    }
-    response.end(file);
-
-  })
-
-}
+  response.writeHead(200, { "Content-Type": "text/html" });
+  // response.write(message);
+  // response.end();
+  var endpoint = request.url;
+  if (endpoint === "/") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile(__dirname + "/../public/index.html", function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
+  } else if (endpoint === "/main.css") {
+    response.writeHead(200, { "Content-Type": "text/css" });
+    fs.readFile(__dirname + "/../public/main.css", function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
+  } else if (endpoint === "/img/image.jpg") {
+    response.writeHead(200, { "Content-Type": "image/jpg" });
+    fs.readFile(__dirname + "/../public/img/image.jpg", function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
+  } else if (endpoint === "/create-post") {
+    var allTheData = "";
+    request.on("data", function(chunkOfData) {
+      // do something
+      allTheData += chunkOfData;
+    });
+    request.on("end", function() {
+      var convertedData = qs.parse(allTheData);
+      console.log(convertedData);
+      console.log('/../public/index.html');
+      response.writeHead(308, {'Location': '/'});
+      console.log(response._header);
+      response.end();
+    });
+  }
 }
 
 // }
